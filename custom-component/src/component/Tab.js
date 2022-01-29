144bdd0836902e7ee2483a6dd4455c3372440940
slobbie <haeru9410@gmail.com>
2022-01-29 20:30:02 +0900

    modal 기능구현

--- custom-component/src/component/Tab.js
+++ custom-component/src/component/Tab.js
@@ -1,30 +1,49 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-export const ModalPopup = ({ onCloes }) => {
+export const ModalPopup = ({ modalSwitch }) => {
+  const onCloseModal = (e) => {
+    if (e.target === e.currentTarget) {
+      modalSwitch();
+    }
+  };
+
+  useEffect(() => {
+    document.body.style.cssText = `
+      position: fixed; 
+      top: -${window.scrollY}px;
+      overflow-y: scroll;
+      width: 100%;`;
+    return () => {
+      const scrollY = document.body.style.top;
+      document.body.style.cssText = '';
+      window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+    };
+  }, []);
+
   return (
-    <di className='ModalPopup'>
-      <button onclick={onCloes}>X</button>
-      <p>Hello</p>
+    <di className='Modaldim' onClick={onCloseModal}>
+      <div className='ModalPopup'>
+        <button onClick={onCloseModal}>X</button>
+        <p>HELLO CODESTATES!</p>
+      </div>
     </di>
   );
 };
 
 const Modal = () => {
-  const [Toggle, setToggle] = useState(false);
+  const [Toggle, setToggle] = useState(false); // 버튼의 상태를 관리 하기위한것
 
-  const onclick = () => {
-    setToggle(true);
+  const modalSwitch = () => {
+    setToggle(!Toggle); // 토글 이벤트
   };
 
-  const onCloes = () => {
-    setToggle(false);
-  };
   return (
     <section className='ContainerBox'>
-      <button className='Modal_btn' onClick={onclick}>
+      <button className='Modal_btn' onClick={modalSwitch}>
         OPEN Modal
       </button>
-      {Toggle ? <ModalPopup onCloes={onCloes} /> : null}
+      {Toggle && <ModalPopup modalSwitch={modalSwitch} />}
+      {/* 모달로 띄우고 싶은 컨텐츠를 부분렌더링을 이용하여 컨트롤한다. */}
     </section>
   );
 };
